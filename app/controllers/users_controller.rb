7b1048c1fe3334a01a6f0d29ad24d7e0609762b7
kossySteve <kossyeze@gmail.com>
2020-09-08 20:21:54 +0100

    corrected linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,26 +8,25 @@ class UsersController < ApplicationController
   end
 
   def create
-     #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
-     @user = User.new(user_params)
-      if @user.save
-        redirect_to new_user_path
-      else
-        render :new
-      end
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
-  
+
   def update
-    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.find(params[:id])
-     if @user.update(user_params)
-       redirect_to user_path(@user)
-     else
-       render :edit
-     end
- end
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
 
   private
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
